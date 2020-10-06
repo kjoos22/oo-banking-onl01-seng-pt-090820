e56af0d692af1f1a68c2cfc9ecec61ef210aefcd
Kristian Joos <kjoos22@gmail.com>
2020-10-06 00:45:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,10 @@
 class Transfer
   attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+  
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
+    @status = pending
   end
 end
